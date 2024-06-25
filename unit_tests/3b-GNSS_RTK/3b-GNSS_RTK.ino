91b51ba0767f945b6b53a4cf804920696ec534a6
jancelin <julien.ancelin@inra.fr>
2024-06-25 18:40:32 +0200

    ex: centipede rtk connection

--- unit_tests/3b-GNSS_RTK/3b-GNSS_RTK.ino
+++ unit_tests/3b-GNSS_RTK/3b-GNSS_RTK.ino
@@ -1,80 +1,128 @@
-/* ------------------------------------------------------
- *
- * @insipration:
- *     http://arduiniana.org/libraries/tinygpsplus/
- * 
- * @brief:  This program feeds TinyGPSPlus object with GNSS_SERIAL buffer data
- *          and dumps updated GNSS time on USB (Serial) port.
- *          
- * @board:
- *    LiLyGo
- * 
- * @GNSS module:
- *    Drotek DP0601 RTK GNSS (XL F9P)  
- *
- * @wiring:
- *      LiLyGo RX2      ->  DP0601 UART1 B3 (TX) 
- *      LiLyGo Vin (5V) ->  DP0601 UART1 B1 (5V)
- *      LiLyGo GND      ->  DP0601 UART1 B6 (GND)
- *      
- * @ports:
- *      Serial (115200 baud)
- *      GNSS_SERIAL (configured baudrate for DP0601)
- *      
- * ------------------------------------------------------
- */
-/* ############################
- * #    GLOBAL DEFINITIONS    #
- * ############################
- */
-/* ###################
- * #    LIBRARIES    #
- * ###################
- */
-#include <TinyGPSPlus.h>
+#include <WiFi.h>
+#include "NTRIPClient.h"
+#include <HardwareSerial.h>
 
-/* #################
- * #    PROGRAM    #
- * #################
- */
-
+HardwareSerial MySerial(1);
 #define PIN_TX 26
 #define PIN_RX 27 
 #define POWER_PIN 25
 
-// GNSS serial port
-HardwareSerial Serialrx(1);
+const char* ssid     = "ici";
+const char* password = "12345678";
+IPAddress server(192, 168, 1, 100);  // IP address of the server
+int port = 80;            
 
-// TinyGPSPlus instance to store GNSS NMEA data (datetim, position, etc.)
-TinyGPSPlus gps;
- 
+char* host = "caster.centipede.fr";
+int httpPort = 2101; //port 2101 is default port of NTRIP caster
+char* mntpnt = "CT";
+char* user   = "rover-gnss-tester";
+char* passwd = "";
+NTRIPClient ntrip_c;
+
+const char* udpAddress = "192.168.1.255";
+const int udpPort = 9999;
+
+int trans = 3;  //0 = serial, 1 = udp, 2 = tcp client, 3 = MySerial, 4 = myserial Choose wich out you want use. for rs232 set 0 et connect tx f9p directly to rs232 module
+
+WiFiUDP udp;
+
 void setup() {
-  pinMode(POWER_PIN, OUTPUT);
-  digitalWrite(POWER_PIN, HIGH);
 
-  // Setup Serial ports for commmunication
+    // POWER_PIN : This pin controls the power supply of the MICRO PCI card
+    pinMode(POWER_PIN, OUTPUT);
+    digitalWrite(POWER_PIN, HIGH);
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serialrx.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
+  delay(500);
+  MySerial.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);//serial port to send rtcm to f9p
   delay(100);
+  MySerial.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
+  delay(100);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println("Requesting SourceTable.");
+  if(ntrip_c.reqSrcTbl(host,httpPort)){
+    char buffer[512];
+    delay(5);
+    while(ntrip_c.available()){
+      ntrip_c.readLine(buffer,sizeof(buffer));
+      Serial.print(buffer); 
+    }
+  }
+  else{
+    Serial.println("SourceTable request error");
+  }
+  Serial.print("Requesting SourceTable is OK\n");
+  ntrip_c.stop(); //Need to call "stop" function for next request.
+  
+  Serial.println("Requesting MountPoint's Raw data");
+  if(!ntrip_c.reqRaw(host,httpPort,mntpnt,user,passwd)){
+    delay(15000);
+    ESP.restart();
+  }
+  Serial.println("Requesting MountPoint is OK");
+
+  
 }
 
 void loop() {
-  // While GNSS_SERIAL buffer is not empty
-  while (Serialrx.available())
-    // Read buffer
-    gps.encode(Serialrx.read());
 
-  // Dump GNSS time (UTC)
-  Serial.print(gps.time.hour());
-  Serial.print(':');
-  Serial.print(gps.time.minute());
-  Serial.print(':');
-  Serial.print(gps.time.second());
-  Serial.print('.');
-  Serial.print(gps.time.centisecond());
-  Serial.print(" - LONG = ");
-  Serial.print(gps.location.lng());
-  Serial.print(" - LAT = ");
-  Serial.print(gps.location.lat());
-  Serial.println();  
+ WiFiClient client;
+ 
+  while(ntrip_c.available())
+    {
+        char ch = ntrip_c.read();        
+        MySerial.print(ch);
+        
+    }
+  while (MySerial.available())
+    {
+    String s = MySerial.readStringUntil('\n');
+    switch (trans)
+      {
+        case 0:  //serial out
+        Serial.println(s);
+        break;
+        case 1:  //udp out
+        udp.beginPacket(udpAddress, udpPort);
+        udp.print(s);
+        udp.endPacket();
+        break;
+        case 2:  //tcp client out
+        if (!client.connect(server, port))
+          {
+            Serial.println("connection failed");
+            return;
+          }
+        client.println(s);
+        while (client.connected())
+          {
+            while (client.available())
+            {
+              char c = client.read();
+              Serial.print(c);
+            }
+          }
+        client.stop();
+        break;
+        case 3:  //MySerial out
+        MySerial.println(s);
+        break;
+        case 4:  //MySerial out
+        MySerial.println(s);
+        break;
+        default:  //mauvaise config
+        Serial.println("mauvais choix ou oubli de configuration");
+        break;
+      }
+    }
 }
--- unit_tests/3b-GNSS_RTK/README.md
+++ unit_tests/3b-GNSS_RTK/README.md
@@ -1,40 +1,17 @@
----
-layout: default
-parent: Tests unitaires
-grand_parent: Satellite Cyclopée
-title: Test de réception du temps GNSS
-nav_order: 6
-has_children: False
----
+# F9p-ntrip-esp32
+Code pour utiliser un esp32 pour injecter directement le rtcm3 dans le f9p et un rs232 ou bluetooth en sortie  
+utile pour fournir une trame nmea corrigée sur une console qui ne gere pas le rtk  
+  
+Librairies
+https://github.com/GLAY-AK2/NTRIP-client-for-Arduino  
+esp32 maj : prend en charge les nouvelles cartes bien plus performante comme esp32-s3 (pas de bluetooth standard), esp32-c3(premier choix) ...  
+https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json  
 
-Test de réception du temps GNSS
-===============================
+Pour une utilisation bluetooth, il faut un esp32 standard. le code pour c3 sera prochainement disponible
 
-## En bref
-Ce programme permet de tester la réception du temps GNSS communiqué par un récepteur GNSS via les trames NMEA `$GPGGA` et `$GPRMC`.
+![Sans titre](https://user-images.githubusercontent.com/32975584/175784306-eab59d33-8d2f-483f-8178-c91edfd0ec41.jpg)  
+or  
 
-## Matériel
-- lilyGO 3.5;
-- Drotek DP0601 RTK GNSS (XL F9P)
+![photo_2022-06-11_14-27-14](https://user-images.githubusercontent.com/32975584/173219745-3c69165b-3083-4c63-ab6b-fc04abe8334d.jpg)
 
-## Bibliothèque
-- `TinyGPSPlus`.
-
-## Inspiration
-Ce programme est inspiré des exemples présents sur le site [Arduiniana](http://arduiniana.org/libraries/tinygpsplus/).
-
-## Ports
-Pour ce montage, le port de communication `Serial` est utilisé pour le debug via USB (moniteur série de l'IDE Arduino) à 115200 baud. <br>
-Le récepteur Drotek DP0601 a été configuré pour diffuser les trames NMEA `$GPGGA` et `$GPRMC` sur son port `UART1`.<br>
-Le lilyGO utilisera son port `Serialrx` pour recevoir les trames NMEA du récepteur GNSS.
-
-## Branchements
-
-Le montage conciste simplement à connecter le port série `Serial5` du Teensy au port série `UART1` du recepteur Drotek.
-
-|LiLyGo|DP0601|
-|------|------|
-|RX|UART1 B3 (TX)|
-|Vin (5V)|UART1 B1 (5V)|
-|GND|UART1 B6 (Gnd)|
-
+![Capture d’écran 2022-08-29 192657](https://user-images.githubusercontent.com/32975584/187261824-5b02ef2c-bc4a-482e-aa8f-ffc1788b9145.png)
