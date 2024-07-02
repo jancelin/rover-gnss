7403ca049a86037180fb0fb60358fd2a83107bde
jancelin <julien.ancelin@inra.fr>
2024-07-02 09:26:03 +0200

    Class Logger les fonctions logmessage

--- lilyGo/lilyGo.ino
+++ lilyGo/lilyGo.ino
@@ -1,4 +1,5 @@
 #include "Config.h"
+#include "Logger.h"
 
 void setup() {
     pinMode(POWER_PIN, OUTPUT);
@@ -78,47 +79,6 @@ void loop() {
     }
 
     handleNTRIPData();
-}
-
-void logMessage(int level, const char* message) {
-    if (level <= LOG_LEVEL) {
-        Serial.println(message);
-    }
-}
-
-void logMessage(int level, const char* message, int value) {
-    if (level <= LOG_LEVEL) {
-        Serial.print(message);
-        Serial.println(value);
-    }
-}
-
-void logMessage(int level, const char* message, const char* value) {
-    if (level <= LOG_LEVEL) {
-        Serial.print(message);
-        Serial.println(value);
-    }
-}
-
-void logMessage(int level, const char* message, float value, int decimals) {
-    if (level <= LOG_LEVEL) {
-        Serial.print(message);
-        Serial.println(value, decimals);
-    }
-}
-
-void logMessage(int level, const char* message, int32_t value) {
-    if (level <= LOG_LEVEL) {
-        Serial.print(message);
-        Serial.println(value);
-    }
-}
-
-void logMessage(int level, const char* message, uint32_t value) {
-    if (level <= LOG_LEVEL) {
-        Serial.print(message);
-        Serial.println(value);
-    }
 }
 
 void locateDevices() {
