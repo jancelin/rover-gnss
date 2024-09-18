6eaac74736e58f92dac0d97f4862fc4d61a76061
Romain Tourte <rtourte@yahoo.fr>
2024-09-18 17:42:16 +0200

    Adding Ntrip client integrated to ESP32

--- libraries/NTRIPClient/NTRIPClient.cpp
+++ libraries/NTRIPClient/NTRIPClient.cpp
@@ -1,57 +1,62 @@
-#include "NTRIPClient.h"
+#include"NTRIPClient.h"
+bool NTRIPClient::reqSrcTbl(char* host,int &port)
+{
+  if(!connect(host,port)){
+      Serial.print("Cannot connect to ");
+      Serial.println(host);
+      return false;
+  }
+  String p;
+  p = String("GET /");
+  p = p + String(" HTTP/1.0\r\n");
+  p = p + String("Host: caster.centipede.fr:2101\r\n");
+  p = p + String("User-Agent: NTRIP Client/buchedv1.01\r\n");
+  p = p + String("Accept: text/html\r\n");
+  p = p + String("User-Agent: Mozilla/5.0\r\n");
+  p = p + String("Connection: keep-alive\r\n");
+  p = p + String("\r\n");
+  print(p);
 
-bool NTRIPClient::reqSrcTbl(char* host, int &port) {
-    if (!connect(host, port)) {
-        Serial.print("Cannot connect to ");
-        Serial.println(host);
+  unsigned long timeout = millis();
+  while (available() == 0) {
+     if (millis() - timeout > 5000) {
+        Serial.println("Client Timeout !");
+        stop();
         return false;
-    }
-    String p;
-    p = String("GET / HTTP/1.0\r\n");
-    p = p + String("Host: caster.centipede.fr:2101\r\n");
-    p = p + String("User-Agent: NTRIP Client/buchedv1.01\r\n");
-    p = p + String("Accept: text/html\r\n");
-    p = p + String("User-Agent: Mozilla/5.0\r\n");
-    p = p + String("Connection: keep-alive\r\n");
-    p = p + String("\r\n");
-    print(p);
-
-    unsigned long timeout = millis();
-    while (available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println("Client Timeout !");
-            stop();
-            return false;
-        }
-        delay(10);
-    }
-    char buffer[12];
-    readLine(buffer, sizeof(buffer));
-    if (strstr(buffer, "SOURCETABLE 200 OK") != NULL) {
-        Serial.print((char*)buffer);
-        return false;
-    }
-    return true;
+     }
+     delay(10);
+  }
+  char buffer[12];
+  readLine(buffer,sizeof(buffer));
+  if(strstr(buffer,"SOURCETABLE 200 OK")!=NULL)
+  {
+    Serial.print((char*)buffer);
+    return false;
+  }
+  return true;
 }
 
-bool NTRIPClient::reqRaw(char* host, int &port, char* mntpnt, char* user, char* psw) {
-    if (!connect(host, port)) return false;
-    String p = "GET /";
-    String auth = "";
+bool NTRIPClient::reqRaw(char* host,int &port,char* mntpnt,char* user,char* psw)
+{
+    if(!connect(host,port))return false;
+    String p="GET /";
+    String auth="";
     Serial.println("Request NTRIP");
-
+    
     p = p + mntpnt + String(" HTTP/1.0\r\n"
-                            "User-Agent: NTRIPClient for Arduino v1.0\r\n");
-
-    if (strlen(user) == 0) {
+        "User-Agent: NTRIPClient for Arduino v1.0\r\n"
+    );
+    
+    if (strlen(user)==0) {
         p = p + String(
-                "Accept: */*\r\n"
-                "Connection: close\r\n"
-        );
-    } else {
-        auth = base64::encode(String(user) + ":" + psw);
+            "Accept: */*\r\n"
+            "Connection: close\r\n"
+            );
+    }
+    else {
+        auth = base64::encode(String(user) + String(":") + psw);
         #ifdef Debug
-        Serial.println(String(user) + ":" + psw);
+        Serial.println(String(user) + String(":") + psw);
         #endif
 
         p = p + String("Authorization: Basic ");
@@ -72,28 +77,29 @@ bool NTRIPClient::reqRaw(char* host, int &port, char* mntpnt, char* user, char* 
         delay(10);
     }
     char buffer[50];
-    readLine(buffer, sizeof(buffer));
-    if (strncmp((char*)buffer, "ICY 200 OK", 10)) {
-        Serial.print((char*)buffer);
-        return false;
+    readLine(buffer,sizeof(buffer));
+    if(strncmp((char*)buffer,"ICY 200 OK",10))
+    {
+      Serial.print((char*)buffer);
+      return false;
     }
     return true;
 }
-
-bool NTRIPClient::reqRaw(char* host, int &port, char* mntpnt) {
-    return reqRaw(host, port, mntpnt, "", "");
+bool NTRIPClient::reqRaw(char* host,int &port,char* mntpnt)
+{
+    return reqRaw(host,port,mntpnt,"","");
 }
+int NTRIPClient::readLine(char* _buffer,int size)
+{
+  int len = 0;
+  while(available()) {
+    _buffer[len] = read();
+    len++;
+    if(_buffer[len-1] == '\n' || len >= size) break;
+  }
+  _buffer[len]='\0';
 
-int NTRIPClient::readLine(char* _buffer, int size) {
-    int len = 0;
-    while (available()) {
-        _buffer[len] = read();
-        len++;
-        if (_buffer[len - 1] == '\n' || len >= size) break;
-    }
-    _buffer[len] = '\0';
-
-    return len;
+  return len;
 }
 
 void NTRIPClient::sendGGA(const char* ggaMessage, const char* host, int port, const char* user, const char* passwd, const char* mntpnt) {
@@ -119,6 +125,13 @@ void NTRIPClient::sendGGA(const char* ggaMessage, const char* host, int port, co
     print(p);
     print(ggaMessage);
     print("\r\n");
-    Serial.println("NTRIPClient sent GGA: " + String(ggaMessage));
+    //Serial.println("NTRIPClient sent GGA: " + String(ggaMessage));
 }
 
+void NTRIPClient::setLastGGA(String gga) {
+    lastGGA = gga;
+}
+
+String NTRIPClient::getLastGGA() {
+    return lastGGA;
+}
---
+++ unit_tests/11-Ntrip/README.md
@@ -0,0 +1,17 @@
+# F9p-ntrip-esp32
+Code pour utiliser un esp32 pour injecter directement le rtcm3 dans le f9p et un rs232 ou bluetooth en sortie  
+utile pour fournir une trame nmea corrigée sur une console qui ne gere pas le rtk  
+  
+Librairies
+https://github.com/GLAY-AK2/NTRIP-client-for-Arduino  
+esp32 maj : prend en charge les nouvelles cartes bien plus performante comme esp32-s3 (pas de bluetooth standard), esp32-c3(premier choix) ...  
+https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json  
+
+Pour une utilisation bluetooth, il faut un esp32 standard. le code pour c3 sera prochainement disponible
+
+![Sans titre](https://user-images.githubusercontent.com/32975584/175784306-eab59d33-8d2f-483f-8178-c91edfd0ec41.jpg)  
+or  
+
+![photo_2022-06-11_14-27-14](https://user-images.githubusercontent.com/32975584/173219745-3c69165b-3083-4c63-ab6b-fc04abe8334d.jpg)
+
+![Capture d’écran 2022-08-29 192657](https://user-images.githubusercontent.com/32975584/187261824-5b02ef2c-bc4a-482e-aa8f-ffc1788b9145.png)
