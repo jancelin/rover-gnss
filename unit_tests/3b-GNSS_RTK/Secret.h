eb4a0e96096e10bb901a0e7cf8490546d4cf100b
Romain Tourte <rtourte@yahoo.fr>
2024-09-24 11:50:42 +0200

    Delete MQTT Config

--- unit_tests/3b-GNSS_RTK/Secret.h
+++ unit_tests/3b-GNSS_RTK/Secret.h
@@ -1,15 +1,6 @@
 #define WIFI_SSID "Your Wifi SSID"
 #define WIFI_PASSWORD "Your Wifi Password"
 
-#define MQTT_SERVER "IP FOR MQTT_SERVER"
-#define MQTT_PORT 1883
-#define MQTT_OUTPUT "your mqtt/output/chanel";
-#define MQTT_INPUT "your mqtt/input/chanel";
-#define MQTT_LOG "your mqtt/log/chanel";
-#define MQTT_USER "MQTT USER"
-#define MQTT_PASSWORD "MQTT PASSWORD IF NEEDED";
-#define MQTT_UUID "UUID FOR MQTT USER";
-
 #define NTRIP_SERVER_HOST "Adresse for Ntrip server"
 #define NTRIP_USER "YOUR NTRIP User"
 #define NTRIP_PASSWORD "YOUR NTRIP Password if neede"
